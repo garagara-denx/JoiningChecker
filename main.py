6b2d8337fe5e9a432c9a2fc4894b2edc855828e5
garagara <sinfu000@gmail.com>
2017-05-22 02:29:34 +0900

    co

--- main.py
+++ main.py
@@ -3,15 +3,14 @@
 
 import requests
 import json
-from requests_oauthlib import OAuth1Session
+import slack_token
 
 #SlackのAPI
-API_TOKEN = "xoxp-15017424657-15066001425-185420635744-2301e7588b711900ebc5f2425392b3af"
 list_url = "https://slack.com/api/users.list"
 profile_url = "https://slack.com/api/users.profile.get"
 
 payload = {
-    'token' : API_TOKEN,
+    'token' : slack_token.API_TOKEN,
 }
 
 def main():
